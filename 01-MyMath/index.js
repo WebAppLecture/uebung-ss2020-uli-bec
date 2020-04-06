e415184fed45841f9079ae592849afdd2b938486
Louis Ritzkowski <louis@pixelwizart.de>
2020-04-06 09:06:58 +0200

    Initial commit

---
+++ 01-MyMath/index.js
@@ -0,0 +1,3 @@
+import { MyMath } from "../1-MyMath/MyMath.js";
+
+window.MyMath = MyMath; // Fügt die Klasse MyMath zum globalen Namespace hinzu, damit ihr sie in der Browserkonsole hernehmen könnt.
---
+++ 03-CSS/src/js/index.js
@@ -0,0 +1,12 @@
+import { SkinChanger } from "./SkinChanger.js";
+
+let skinStyle = document.querySelector("#skin"),
+    skins = ["gold","peach","basic","win95","mech"];
+
+window.skinChanger = new SkinChanger(skinStyle, skins, "./src/css/");
+
+document.querySelector(".next").addEventListener("click", () => skinChanger.next());
+document.querySelector(".previous").addEventListener("click", () => skinChanger.previous());
+
+skinChanger.activeSkin = "basic";
+
